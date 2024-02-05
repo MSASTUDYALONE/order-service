183fafbcf6bf8f24ca70d288881fdbe5fd9b55a5
sesam-me <lsi6601@gmail.com>
2024-02-05 17:54:52 +0900

    Feat: kafka 설정

---
+++ src/main/java/com/example/orderservice/messagequeue/kafkaProducerConfig.java
@@ -0,0 +1,33 @@
+package com.example.orderservice.messagequeue;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class kafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, String> producerFactory() { // 접속 정보 설정
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // KEY 풀어주기
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // VALUE 풀어주기
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean //데이터 전달하는 용도의 카프카템플릿
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
